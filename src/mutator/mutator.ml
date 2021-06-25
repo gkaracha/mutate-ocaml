eabca85ec6d882f464637c8b3475e7d5d82c0350
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-25 13:18:40 +0200

    add non-raw pretty printer

--- src/mutator/mutator.ml
+++ src/mutator/mutator.ml
@@ -1,5 +1,8 @@
 let parse_ml_file (src_file : string) : Parsetree.structure =
   Pparse.parse_implementation ~tool_name:"ocamlc" src_file
 
+let print_raw_ast (ast: Parsetree.structure) : unit =
+  Format.fprintf Format.std_formatter "%a@." Printast.implementation ast
+
 let print_ast (ast: Parsetree.structure) : unit =
-  Format.fprintf Format.std_formatter "%a@." Printast.implementation ast;
+  Format.fprintf Format.std_formatter "%a@." Pprintast.structure ast
