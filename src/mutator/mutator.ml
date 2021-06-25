45ad8b1f378b70fe891c7be3e57008caf706aacc
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-25 14:24:32 +0200

    Start setting up some rules.

--- src/mutator/mutator.ml
+++ src/mutator/mutator.ml
@@ -11,31 +11,73 @@ let print_raw_ast (ast: Parsetree.structure) : unit =
 let print_ast (ast: Parsetree.structure) : unit =
   Format.fprintf Format.std_formatter "%a@." Pprintast.structure ast
 
+(** Replacement rules for closed expressions. *)
+type expression_replacement_rule =
+  { from_expr: Parsetree.expression_desc; to_expr: Parsetree.expression_desc; }
 
-(*
-Mutations to consider
-~~~~~~~~~~~~~~~~~~~~~
-* Replace true by false
-* Replace false by true
+let rule_true_false =
+  {
+    from_expr = Parsetree.Pexp_construct (Location.mknoloc (Longident.Lident "true" ), None);
+    to_expr   = Parsetree.Pexp_construct (Location.mknoloc (Longident.Lident "false"), None);
+  }
 
-* Replace (head :: tail) by tail.
-* Replace (head :: tail) by [head].
-* Replace (list1 @ list2) by list1.
-* Replace (list1 @ list2) by list2.
-* Replace (list1 @ list2) by (list2 @ list1).
+let rule_false_true =
+  {
+    from_expr = Parsetree.Pexp_construct (Location.mknoloc (Longident.Lident "false"), None);
+    to_expr   = Parsetree.Pexp_construct (Location.mknoloc (Longident.Lident "true" ), None);
+  }
 
-* Replace && by ||.
-* Replace || by &&.
+let rule_and_or =
+  {
+    from_expr = Parsetree.Pexp_ident (Location.mknoloc (Longident.Lident "&&"));
+    to_expr   = Parsetree.Pexp_ident (Location.mknoloc (Longident.Lident "||"));
+  }
 
-* Reorder pattern matching equations.
-* Remove pattern matching equations.
+let rule_or_and =
+  {
+    from_expr = Parsetree.Pexp_ident (Location.mknoloc (Longident.Lident "||"));
+    to_expr   = Parsetree.Pexp_ident (Location.mknoloc (Longident.Lident "&&"));
+  }
 
-Replace an integer constant N by one of {0, 1, -1, N+1, N-1}.
-Replace an arithmetic / relational / logical operator by another in the same class.
-Negate a conditional.
+(* TODO: add more rules here (Replace an arithmetic / relational / logical
+ * operator by another in the same class.) *)
 
-Type-aware
-~~~~~~~~~~
-* Replace a random list by the empty list [].
-
+(* Template matching *)
+(* TODO: Figure out how to represent template matching nicely. Perhaps via
+ * functions
+ *
+ * - Replace (head :: tail) by tail.
+ * - Replace (head :: tail) by [head].
+ * - Replace (list1 @ list2) by list1.
+ * - Replace (list1 @ list2) by list2.
+ * - Replace (list1 @ list2) by (list2 @ list1).
+ * - Replace an integer constant N by one of {0, 1, -1, N+1, N-1}.
+ * - Negate a conditional.
 *)
+(* TODO: Perhaps have only two kinds of rules: matching and equality. The
+ * former have holes in them, while the second are closed expressions. *)
+(* TODO: Type-aware: replace a random list by the empty list. *)
+
+(* TODO: Should populate this from a JSON input or something. *)
+type mutation_configuration =
+  {
+    (* Whether to negate the condition in if-then-else expressions. *)
+    negate_conditionals : bool;
+
+    (* Whether to reorder clauses in pattern matching expressions. *)
+    reorder_match_clauses : bool;
+
+    (* Whether to remove clauses in pattern matching expressions. *)
+    remove_match_clauses : bool;
+
+    (* Rules for replacing closed expressions by other closed expressions. *)
+    replacement_rules : expression_replacement_rule list;
+  }
+
+(** Default mutation configuration: do nothing. *)
+let default_configuration =
+  { negate_conditionals = false;
+    reorder_match_clauses = false;
+    remove_match_clauses = false;
+    replacement_rules = [];
+  }
