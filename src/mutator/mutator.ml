6dd8ac1ac3c6e45c25b542eb09f73c697bcb12f0
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-28 11:21:06 +0200

    Add some more (untested) rules

--- src/mutator/mutator.ml
+++ src/mutator/mutator.ml
@@ -12,44 +12,110 @@ let print_ast (ast: Parsetree.structure) : unit =
   Format.fprintf Format.std_formatter "%a@." Pprintast.structure ast
 
 (* For now a rule is a function from an expression to an expression. *)
-type mutation_rule = Parsetree.expression -> Parsetree.expression option
+type mutation_rule = (Parsetree.expression -> Parsetree.expression option)
 
-let rule_true_false expr =
+(** Replace true by false. *)
+let rule_true_false (expr: Parsetree.expression) : Parsetree.expression option =
   let open Parsetree in
   match expr.pexp_desc with
   | Pexp_construct ({ txt = Longident.Lident "true";  loc = loc; }, None) ->
     Some { expr with pexp_desc = Pexp_construct ({ txt = Longident.Lident "false"; loc = loc; }, None) }
   | _ -> None
 
-let rule_false_true expr =
+(** Replace false by true. *)
+let rule_false_true (expr: Parsetree.expression) : Parsetree.expression option =
   let open Parsetree in
   match expr.pexp_desc with
   | Pexp_construct ({ txt = Longident.Lident "false";  loc = loc; }, None) ->
     Some { expr with pexp_desc = Pexp_construct ({ txt = Longident.Lident "true"; loc = loc; }, None) }
   | _ -> None
 
-let rule_and_or expr =
+(** Replace and (&&) by or (||). *)
+let rule_and_or (expr: Parsetree.expression) : Parsetree.expression option =
   let open Parsetree in
   match expr.pexp_desc with
   | Pexp_ident ({ txt = Longident.Lident "&&"; loc = loc; }) ->
     Some { expr with pexp_desc = Pexp_ident ({ txt = Longident.Lident "||"; loc = loc; }) }
   | _ -> None
 
-let rule_or_and expr =
+(** Replace or (||) by and (&&). *)
+let rule_or_and (expr: Parsetree.expression) : Parsetree.expression option =
   let open Parsetree in
   match expr.pexp_desc with
   | Pexp_ident ({ txt = Longident.Lident "||"; loc = loc; }) ->
     Some { expr with pexp_desc = Pexp_ident ({ txt = Longident.Lident "&&"; loc = loc; }) }
   | _ -> None
 
+(** Replace (e1 @ e2) by (e2 @ e1). *)
+let rule_append_swap_args (expr: Parsetree.expression) : Parsetree.expression option =
+  let open Parsetree in
+  match expr.pexp_desc with
+  | Pexp_apply (fn, [(lbl1, e1); (lbl2, e2)]) ->
+     begin match fn.pexp_desc with
+     | Pexp_ident ({ txt = Longident.Lident "@"; loc = _loc; }) ->
+       Some { expr with pexp_desc = Pexp_apply (fn, [(lbl1, e2); (lbl2, e1)]) }
+     | _ -> None
+     end
+  | _ -> None
+
+(** Replace (e1 @ e2) by (e1). *)
+let rule_append_keep_first (expr: Parsetree.expression) : Parsetree.expression option =
+  let open Parsetree in
+  match expr.pexp_desc with
+  | Pexp_apply (fn, [(_lbl1, e1); _]) ->
+     begin match fn.pexp_desc with
+     | Pexp_ident ({ txt = Longident.Lident "@"; loc = _loc; }) -> Some e1
+     | _ -> None
+     end
+  | _ -> None
+
+(** Replace (e1 @ e2) by (e2). *)
+let rule_append_keep_second (expr: Parsetree.expression) : Parsetree.expression option =
+  let open Parsetree in
+  match expr.pexp_desc with
+  | Pexp_apply (fn, [_; (_lbl2, e2)]) ->
+     begin match fn.pexp_desc with
+     | Pexp_ident ({ txt = Longident.Lident "@"; loc = _loc; }) -> Some e2
+     | _ -> None
+     end
+  | _ -> None
+
+(** Replace (head :: tail) by ([head]). *)
+let rule_list_head (expr: Parsetree.expression) : Parsetree.expression option =
+  let open Parsetree in
+  match expr.pexp_desc with
+  | Pexp_construct ({ txt = Longident.Lident "::"; loc = loc; }, Some args) ->
+    begin match args.pexp_desc with
+    | Pexp_tuple ([e1; e2]) ->
+      Some
+        { expr with pexp_desc =
+            Pexp_construct
+              ( { txt = Longident.Lident "::"; loc = loc; },
+                Some
+                  { args with pexp_desc =
+                      Pexp_tuple ([e1; { e2 with pexp_desc = Pexp_construct ({ txt = Longident.Lident "true"; loc = e2.pexp_loc; }, None) } ])
+                  }
+              )
+        }
+    | _ -> None
+    end
+  | _ -> None
+
+(** Replace (head :: tail) by (tail). *)
+let rule_list_tail (expr: Parsetree.expression) : Parsetree.expression option =
+  let open Parsetree in
+  match expr.pexp_desc with
+  | Pexp_construct ({ txt = Longident.Lident "::"; loc = _loc; }, Some args) ->
+    begin match args.pexp_desc with
+    | Pexp_tuple ([_; e2]) -> Some e2
+    | _ -> None
+    end
+  | _ -> None
+
+
 
 (* TODO: add more rules here
  * - Replace an arithmetic / relational / logical operator by another in the same class.
- * - Replace (head :: tail) by tail.
- * - Replace (head :: tail) by [head].
- * - Replace (list1 @ list2) by list1.
- * - Replace (list1 @ list2) by list2.
- * - Replace (list1 @ list2) by (list2 @ list1).
  * - Replace an integer constant N by one of {0, 1, -1, N+1, N-1}.
  * - Negate a conditional.
 *)
@@ -106,6 +172,8 @@ let () = Ast_mapper.register "ppx_test" (fun _ -> mapper_from_rules all_rules)
 *)
 
 let example = true && false && (1 = 4)
+
+let example2 = [1] @ []
 
 (* More about ppx mappers here:
  * https://ocaml.org/api/compilerlibref/Ast_mapper.html *)
